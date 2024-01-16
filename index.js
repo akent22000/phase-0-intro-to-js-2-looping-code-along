02cd6df4ea8bcb41d538ff6c72c139c0172b6748
amandalocher <akent22000@gmail.com>
2024-01-16 14:02:51 -0500

    loop test

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+
+
+function countDown(){
+    let i;
+    for (i=10; i>=0; i--){
+      console.log(i);
+    } 
+    if (i == 0){
+      console.log("Stopped")
+    }
+  }
+  
+  countDown();
+
+
